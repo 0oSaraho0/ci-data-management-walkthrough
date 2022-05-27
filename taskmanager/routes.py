86a8ef4e64b34b78703bb8ca5aaba7b9ffe6bda3
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-05-27 08:55:10 +0000

    finish lesson Updating categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -61,3 +61,79 @@ def add_category():
         return redirect(url_for("categories"))
     return render_template("add_category.html")
 
+"""
+Let's go over to the routes.py file, and we are going to create a new function.
+For the app route URL, let's call this "/edit_category", and once again, we 
+will be using this as adual-purpose for both the "GET" and "POST" methods.
+The function name will match, so that will be "edit_category". To start with,
+we will only focus on the "GET" method, which will get the template, and render
+it on screen for us. We can simply return render_template using the new file 
+we created, "edit_category.html".However, we need some sort of mechanism for
+the app to know which specific category we intend to update. In order to
+understand this, let's open the template for all categories, which contains
+the for-loop we built in the last video.
+"""
+
+"""
+Now, we can head back over to the routes.py file, and since we've given an argument of
+'category_id' when clicking the 'Edit' button, this also needs to appear in our app.route.
+These types of variables being passed back into our Python functions must be wrapped
+inside of angle-brackets within the URL.
+We know that all of our primary keys will be integers, so we can cast this as an 'int'.
+We also need to pass the variable directly into the function as well, so we have the
+value available to use within this function.
+If you have attempted to save these changes and load the page, then you're going to get an error.
+This is a very common error, and something that all developers should know how to understand,
+so let's save everything, and load the live preview.
+Once that's loaded, navigate to the Categories page,
+"""
+
+"""
+try clicking on one of the Edit buttons, and you'll notice the Werkzeug Error.
+"Could not build url endpoint 'edit_category'. Did you forget to specify values ['category_id']?"
+The really fantastic thing with any Flask error, is that it will always tell you exactly
+which file and line number is causing the specific error.
+Normally, this can be found towards the bottom of the error lines, and you want to look for
+the code in the blue rows that matches your own code.
+As you can see here, we're calling the URL for the edit_category function, which is listed
+on the edit_category.html template, from line 7.
+Essentially what happened is once we added the primary key of ID into our app.route function,
+it will now always expect this for any link that calls this function.
+Let's go back to the edit_category template, and sure enough on line 7 we have the url_for method.
+"""
+
+"""
+In order for this function to know which specific category to load, we need to attempt to find
+one in the database using the ID provided from the URL.
+The template is expecting a variable 'category', so let's create that new variable now.
+Using the imported Category model from the top of the file, we need to query the database,
+and this time we know a specific record we'd like to retrieve.
+There's a SQLAlchemy method called '.get_or_404()', which takes the argument of 'category_id'.
+What this does is query the database and attempts to find the specified record using the data
+provided, and if no match is found, it will trigger a 404 error page.
+Now, we can pass that variable into the rendered template, which is expecting it to be called
+'category', and that will be set to the defined 'category' variable above.
+The page should load now without any errors, however, if you notice, it doesn't show us
+the current value of our category, and the form doesn't do anything just yet.
+"""
+
+"""
+The final step now, is to add the "POST" functionality so the database actually gets updated with the requested changes.
+Back within our routes.py file, just after the 'category' variable being defined, let's
+conditionally check if the requested method is equal to "POST".
+If so, then we want to update the category_name for our category variable, and we'll set that
+to equal the form's name-attribute of 'category_name'.
+After that, we need to commit the session over to our database.
+Finally, if that's all successful, we should redirect the users back to the categories
+function, which will display all of them in the cards once again.
+Save all of your changes, and let's go back to the live preview.
+"""
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
