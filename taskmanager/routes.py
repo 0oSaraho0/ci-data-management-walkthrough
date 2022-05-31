0ed3af3c474e85e844737fd64a51e242c7165202
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-05-31 13:00:44 +0000

    testing for bug in routs.py

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -59,6 +59,7 @@ def add_task():
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
 
+
 @app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
     task = Task.query.get_or_404(task_id)
@@ -67,10 +68,11 @@ def edit_task(task_id):
         task.task_name = request.form.get("task_name")
         task.task_description = request.form.get("task_description")
         task.is_urgent = bool(True if request.form.get("is_urgent") else False)
-        task.due_date = request.form.get("due_date"),
+        task.due_date = request.form.get("due_date")
         task.category_id = request.form.get("category_id")
         db.session.commit()
-    return render_template("edit_task.html",task=task, categories=categories)
+    return render_template("edit_task.html", task=task, categories=categories)
+
 
 @app.route("/delete_task/<int:task_id>")
 def delete_task(task_id):
