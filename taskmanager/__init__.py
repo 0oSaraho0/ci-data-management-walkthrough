9d1007b2ac9af56edc14825d196eda8c07a2cf0b
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-05-31 13:21:59 +0000

    check sourse code for routes.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,7 +9,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
-    
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
